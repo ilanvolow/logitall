d1d46e770279ca5e6ff90f50e1511179731497a3
Ilan Volow <ilanvolow@gmail.com>
2020-03-08 03:29:34 -0400

    Adding support for anonymous functions

--- TestClass.ts
+++ TestClass.ts
@@ -1,6 +1,30 @@
 
 class MyClass {
     myMethod() {
-        let myVar = 5;
+        let myVar = 'Bunny';
+        console.log(myVar);
+    }
+
+    callbackMethod() {
+        const fatArrowFunction = ((xVar, yVar) => {
+            let myVar = 'Bubba';
+            console.log(myVar);
+        });
+
+        fatArrowFunction(5, 7);
+    }
+
+    callAnonymous() {
+        const anonFunction = function(x, y) {
+            let myVar = 'Babushka';
+            console.log(myVar);
+        }
+
+        anonFunction(5, 7);
     }
 }
+
+let myNewClass = new MyClass();
+myNewClass.myMethod();
+myNewClass.callbackMethod();
+myNewClass.callAnonymous();
